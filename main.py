b859f2f8c08d83a1f4df0bd7288d4b0fc3bccabf
sunilsky92 <44274027+sunilsky92@users.noreply.github.com>
2024-09-22 02:47:48 +0530

    changed dashboard name

--- main.py
+++ main.py
@@ -105,7 +105,7 @@ def main():
 
 if __name__ == "__main__":
     # Run Steamlit App
-    st.set_page_config(page_title='API Stats Dashboard', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
+    st.set_page_config(page_title='Dashboard', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
     #st.set_option('deprecation.showfileUploaderEncoding', False)
     #st.set_option('deprecation.showPyplotGlobalUse', False)
     main()
