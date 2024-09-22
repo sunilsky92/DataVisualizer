f011c18ebb066b8f0dd3e7a2b36ed3e191464e58
sunilsky92 <44274027+sunilsky92@users.noreply.github.com>
2024-09-22 19:02:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import pandas as pd
 #import win32com.client
 from datetime import datetime
 #import xlsxwriter
+import numpy as np
+#import matplotlib.pyplot as plt
+#import altair as alt
 
 
 #Function to upload CSV file
@@ -32,33 +35,33 @@ def load_csv_data(file, data=None):
     return data
 
 
-def getMailsFromFolder(folder_name = None, subfolder_name = None):
-    outlook = win32com.client.Dispatch("Outlook.Application")
-    namespace = outlook.GetNamespace("MAPI")
-    inbox = namespace.GetDefaultFolder(6) # "6" refers to the inbox
-    folder = None
-    for f in inbox.Folders:
-        if f.Name == folder_name:
-            folder = f
-            break
-    if folder is None:
-        st.write(f'folder not found: {folder_name} in Inbox, Checking in default folder Inbox')
-        messages = inbox.Items
-    else:
-        if subfolder_name is None:
-            messages = folder.Items
-        else:
-            # Checking subfolder_name
-            subfolder = None
-            for sf in folder.Folders:
-                if sf.Name == subfolder_name:
-                    subfolder = sf
-                    break
-            messages = subfolder.Items
-    # Last x days mails in inbox
-    messages.Sort("[ReceivedTime]", True)
-    #messages = messages.Restrict("[ReceivedTime] >= '09/09/2024'")
-    st.write(messages)
+#def getMailsFromFolder(folder_name = None, subfolder_name = None):
+#    outlook = win32com.client.Dispatch("Outlook.Application")
+#    namespace = outlook.GetNamespace("MAPI")
+#    inbox = namespace.GetDefaultFolder(6) # "6" refers to the inbox
+#    folder = None
+#    for f in inbox.Folders:
+#        if f.Name == folder_name:
+#            folder = f
+#            break
+#    if folder is None:
+#        st.write(f'folder not found: {folder_name} in Inbox, Checking in default folder Inbox')
+#        messages = inbox.Items
+#    else:
+#        if subfolder_name is None:
+#            messages = folder.Items
+#        else:
+#            # Checking subfolder_name
+#            subfolder = None
+#            for sf in folder.Folders:
+#                if sf.Name == subfolder_name:
+#                    subfolder = sf
+#                    break
+#            messages = subfolder.Items
+#    # Last x days mails in inbox
+#    messages.Sort("[ReceivedTime]", True)
+#    #messages = messages.Restrict("[ReceivedTime] >= '09/09/2024'")
+#    st.write(messages)
 
 
 def main():
@@ -82,6 +85,8 @@ def main():
     csv_data = csv_data.fillna(0)
     # plot the graph
     if csv_data is not None and not csv_data.empty:
+        with st.expander('See Header'):
+            st.write(csv_data.head())
         time_field = st.selectbox('Select Time Field', csv_data.columns)
         if time_field not in csv_data.columns:
             st.write(f"The dataset must contain a {time_field} column.")
@@ -94,18 +99,38 @@ def main():
                 plt_data = pd.DataFrame(csv_data[[time_field] + selected_fields])
                 plt_data.set_index(time_field, inplace=True, drop=True, append=False, verify_integrity=False)
                 #st.write(plt_data)
-                st.line_chart(plt_data, use_container_width=True, height=500, width=30)
-            else:
-                st.write("Please select at least one column to plot.")
+                # Tab
+                tab1, tab2, tab3 = st.tabs(['Line Plot', 'Bar Plot', 'Scatter Plot'])
+                with tab1:
+                    st.write('Line Plot')
+                    st.line_chart(plt_data, use_container_width=True, height=500, width=30) 
+                with tab2:
+                    st.write('Bar Plot')
+                    st.bar_chart(plt_data, use_container_width=True, height=500, width=30)
+                with tab3:
+                    st.write('Scatter Plot')
+                    #st.write(plt_data[plt_data.columns[0]].values, plt_data[plt_data.columns[1]].values, kind='scatter', use_container_width=True, height=500, width=30)
+                    st.scatter_chart(plt_data, use_container_width=True, height=500, width=30)
+                #with tab4:
+                #    st.write('Altair Plot')
+                #    #st.write(plt_data)
+                #    if len(selected_fields) > 1:
+                #        chart = alt.Chart(plt_data).mark_circle().encode(
+                #            x=alt.X(time_field, type='temporal'),
+                #            y=alt.Y(selected_fields[0], type='quantitative'),
+                #            size = alt.Size(selected_fields[1], type='quantitative'),
+                #            color=alt.Color(selected_fields[1], type='nominal')
+                #        ).properties(width=600, height=300)          #.interactive()
+                #        st.altair_chart(chart, use_container_width=True)
+                #    else:
+                #        st.write("Please select at least two fields for the Altair plot.")
     else:
         st.write("Please upload the data to plot the graph")
-
-
 
 
 if __name__ == "__main__":
     # Run Steamlit App
     st.set_page_config(page_title='Dashboard', page_icon=':bar_chart:', layout='wide', initial_sidebar_state='auto')
     #st.set_option('deprecation.showfileUploaderEncoding', False)
-    #st.set_option('deprecation.showPyplotGlobalUse', False)
+    st.set_option('deprecation.showPyplotGlobalUse', False)
     main()
